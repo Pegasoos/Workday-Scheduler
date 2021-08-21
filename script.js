602fc8441f1e8c9733c7222e7896f2a6bee343ff
Pegasoos <circumambulation24@yahoo.com>
2021-08-20 20:04:52 -0400

    Script to change class based on time

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-console.log(localStorage.getItem("task"))
 
 var saveButton = $("i");
 
 var currentDate = moment().format("dddd, MMMM Do YYYY");
-
+//loop to create moments, pass ids as a parameter, change classes using isBefore,isSame,isAfter, compare with currentHour?
 $("#currentDay").text(currentDate);
+//moment formatted to fit id formatting
+ let currentHour = moment().utc().format()
 
+ let hourBlocks = $("textarea")
+ console.log($(hourBlocks))
+//check moment doc for correct moment construction, use ISO or RFC if possible
+ for(i = 0; i<$(hourBlocks).length;i++){
+     let blockTime = moment().hours(9 + i).utc().format()
+
+     if(moment(blockTime).isSame(moment(currentHour))){
+         $(hourBlocks[i]).addClass(".present")
+     }
+     else if(moment(blockTime).isAfter(moment(currentHour))){
+        $(hourBlocks[i]).addClass(".future")
+     }
+     else{
+        $(hourBlocks[i]).addClass("past")
+     } 
+ }
 function saveTask(){
     //code to select text of each time block
     var taskItem = this.parentNode.previousElementSibling.children[0];
@@ -15,8 +32,6 @@ function saveTask(){
     //starageNum creates seperate localStorage variables that match seperate time block ids
     $(taskItem).val(localStorage.getItem("task" + storageNum))
 };
-    
-  
 saveButton.on("click", saveTask);
 
  $("textarea").each(function(){
@@ -24,4 +39,5 @@ saveButton.on("click", saveTask);
      $(this).val(localStorage.getItem("task" + blockNum))
  }) 
 
+ 
 
